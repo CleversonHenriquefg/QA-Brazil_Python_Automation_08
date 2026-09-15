b80929002daf8ba0ae49329adecddbfea1574728
CleversonHenriquefg <chenriquefg25@gmail.com>
2026-09-15 20:20:00 -0300

    feat: adiciona testes Urban Routes do Sprint 8

---
+++ data.py
@@ -0,0 +1,12 @@
+# URL atual do servidor Urban Routes.
+URBAN_ROUTES_URL = (
+    "https://cnt-877a58c8-4f6b-4547-89e3-1de174128f41.containerhub.tripleten-services.com/?lng=pt"
+)
+
+# Dados usados nos testes.
+ADDRESS_FROM = "East 2nd Street, 601"
+ADDRESS_TO = "1300 1st St"
+PHONE_NUMBER = "+12025550123"
+CARD_NUMBER = "1234 5678 9100"
+CARD_CODE = "111"
+MESSAGE_FOR_DRIVER = "Pare no bar de sucos"
